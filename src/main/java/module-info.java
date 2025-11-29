763595e0678664cb047f05b0938d840099b0c4f9
taker1974 <kostus.online.1974@yandex.ru>
2025-11-29 22:54:36 +0300

    module name changed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,7 @@
  * @author Konstantin Terskikh, kostus.online.1974@yandex.ru, 2025
  * @since 2.0.5
  */
-module tk.log.utils {
+module ru.spb.tksoft.utils.log {
     requires transitive org.slf4j;
 
     exports ru.spb.tksoft.utils.log;
