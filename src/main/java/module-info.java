bc525406cf8a9e8039a7df302832f75ba1e56067
taker1974 <kostus.online.1974@yandex.ru>
2025-11-29 22:41:16 +0300

    module name manifested/stabilized

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,13 @@
 /**
  * Module descriptor for tk-log-utils.
  * 
- * <p>
  * This module provides extended logging utilities with thread safety optimizations.
  * 
- * @author Konstantin Terskikh
+ * @author Konstantin Terskikh, kostus.online.1974@yandex.ru, 2025
  * @since 2.0.5
  */
-module ru.spb.tksoft.tk.log.utils {
+module tk.log.utils {
     requires transitive org.slf4j;
 
     exports ru.spb.tksoft.utils.log;
 }
-
